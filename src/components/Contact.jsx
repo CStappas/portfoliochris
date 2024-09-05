06619b9d13463cc5b9f4d6f65d1290292120d84e
bigflopz <chris.stappas@gmail.com>
2024-09-04 21:03:06 -0400

    added submit form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,102 @@
-import React from 'react'
-import "./Contact.css"
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
+import "./Contact.css";
 
-function Contact() {
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .send(
+        "service_g35ri6p",
+        "template_p08hk6d",
+        {
+          name: formData.name,
+          email: formData.email,
+          message: formData.message,
+        },
+        "ukIIoOvI5NBmTB1Rz"
+      )
+      .then((response) => {
+        console.log("SUCCESS!", response.status, response.text);
+        alert("Email sent successfully!");
+      })
+      .catch((error) => {
+        console.error("FAILED...", error);
+        alert("Failed to send email.");
+      });
+
+    setFormData({
+      name: "",
+      email: "",
+      message: "",
+    });
+  };
+
   return (
-    <div id="contact" className='section'>Contact</div>
-  )
-}
+    <div id="contact" className="section">
+    <form onSubmit={handleSubmit}  className="contact-form">
+      <div className="form-group">
+        <label htmlFor="name" className="form-label">
+          Name:
+        </label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          className="form-input"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="email" className="form-label">
+          Email:
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          className="form-input"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="message" className="form-label">
+          Message:
+        </label>
+        <textarea
+          id="message"
+          name="message"
+          className="form-textarea"
+          value={formData.message}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <button type="submit" className="form-button">
+        Submit
+      </button>
+    </form>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -7,70 +7,73 @@ const Section = () => {
   const introtext = "Hi I'm Christos, a front-end developer.";
 
   return (
-    <div>
-    <div>
-      <div id="title">
-      <TypewriterEffect text={introtext} />
-    </div>
-    <div id="about"className="section">
-      <div id="content-box">
-        <div id="title-box">
-          <span id="text">About Me</span>
+    <div >
+      <div>
+        <div id="title">
+          <TypewriterEffect text={introtext} />
         </div>
-        <div id="description">
-          <p>
-            Hey, I'm Christos, a computer enthusiast turned software developer.
-            I've been hooked on computers since childhood, and that early
-            fascination sparked my journey into coding.
-          </p>
+        <div id="about" className="section">
+          <div id="content-box">
+            <section class="about-me">
+              <div class="title-box">
+                <span class="title-text">About Me</span>
+              </div>
+              <div class="description">
+                <p>
+                  Hey, I'm Christos, a computer enthusiast turned software
+                  developer. I've been hooked on computers since childhood, and
+                  that early fascination sparked my journey into coding.
+                </p>
 
-          <p>
-            <strong>Why Code?</strong> For me, coding is about turning ideas
-            into reality. I thrive on solving problems and enjoy the art of
-            crafting seamless digital experiences.
-          </p>
+                <p>
+                  <strong>Why Code?</strong> For me, coding is about turning
+                  ideas into reality. I thrive on solving problems and enjoy the
+                  art of crafting seamless digital experiences.
+                </p>
 
-          <p>
-            <strong>Skills Snapshot</strong>
-            <ul>
-              <li>
-                Proficient in JavaScript (React) and modern front-end
-                development practices.
-              </li>
-              <li>
-                Experienced in building responsive UIs, working with RESTful
-                APIs, and managing state effectively.
-              </li>
-              <li>
-                Skilled in front-end tools and technologies like HTML5, CSS3,
-                and modern JavaScript (ES6+).
-              </li>
-              <li>
-                Team player with a knack for elegant solutions and clean code
-                practices.
-              </li>
-            </ul>
-          </p>
+                <p>
+                  <strong>Skills Snapshot</strong>
+                  <ul class="skills-list">
+                    <li>
+                      Proficient in JavaScript (React) and modern front-end
+                      development practices.
+                    </li>
+                    <li>
+                      Experienced in building responsive UIs, working with
+                      RESTful APIs, and managing state effectively.
+                    </li>
+                    <li>
+                      Skilled in front-end tools and technologies like HTML5,
+                      CSS3, and modern JavaScript (ES6+).
+                    </li>
+                    <li>
+                      Team player with a knack for elegant solutions and clean
+                      code practices.
+                    </li>
+                  </ul>
+                </p>
 
-          <p>
-            <strong>Projects</strong> From Netflix Clones to Pokemon Pokedex ,
-            my work reflects my dedication to creating meaningful digital
-            solutions.
-          </p>
+                <p>
+                  <strong>Projects</strong> From Netflix Clones to Pokemon
+                  Pokedex, my work reflects my dedication to creating meaningful
+                  digital solutions.
+                </p>
 
-          <p>
-            <strong>Beyond the Code</strong> When I'm not coding, I'm exploring
-            the latest tech trends and contributing to the developer community.
-            Let's connect and explore the possibilities of code!
-          </p>
+                <p>
+                  <strong>Beyond the Code</strong> When I'm not coding, I'm
+                  exploring the latest tech trends and contributing to the
+                  developer community. Let's connect and explore the
+                  possibilities of code!
+                </p>
 
-          <p>Cheers, Christos</p>
+                <p>Cheers, Christos</p>
+              </div>
+            </section>
+          </div>
         </div>
+        <Projects />
       </div>
-    </div>
-    <Projects/>
-    </div>
-    <Contact/>
+      <Contact />
     </div>
   );
 };
