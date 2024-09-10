8ab5ddf31ba51aeca1f3e0b73a1734756dd0675e
bigflopz <chris.stappas@gmail.com>
2024-09-09 23:45:55 -0400

    fixed href

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -119,6 +119,7 @@ const Contact = () => {
           <img
             src={Linkedin}
             alt="LinkedIn"
+            className="social-icon"
             style={{ width: '50px',padding: "10px"}}
           />
         </a>
